900cf629747c0bcf63712ca03b12f4b317502179
Hahatek <piotrus.guzowski@gmail.com>
2025-11-05 16:24:51 +0100

    small change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/Weather-App/",
+  build: { outDir: "docs" },
   plugins: [
     tailwindcss(),
     react(),
